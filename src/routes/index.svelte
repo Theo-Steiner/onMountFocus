5d50114db5f9298694ec0747b6935d5b3729840c
Theo Steiner <theosteiner@gmail.com>
2021-09-23 17:19:06 +0900

    Show bug

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<h1>Welcome to SvelteKit's unexpected onMount behavior</h1>
+<a href="/not-working">Not working scroll & focus demo</a>
+<a href="/workaround">Working scroll and focus demo using setTimeout()</a>
+
+<style>
+  :global(body) {
+    background-color: tan;
+    display: flex;
+    flex-direction: column;
+  }
+
+  a {
+    display: block;
+    margin: 20px;
+  }
+</style>
---
+++ src/routes/workaround.svelte
@@ -0,0 +1,20 @@
+<script>
+  const focusAndScroll = (node) => {
+    setTimeout(() => node.focus(), 0);
+    setTimeout(() => node.scrollIntoView(), 0);
+  };
+</script>
+
+<div>They see me (sc)rollin'...</div>
+<div style="background-color: peru;">
+  <label for="input">Focus! </label>
+  <input use:focusAndScroll id="input" type="text" />
+</div>
+<div style="background-color: teal;">They focusin'</div>
+
+<style>
+  div {
+    background-color: hotpink;
+    height: 80vh;
+  }
+</style>
